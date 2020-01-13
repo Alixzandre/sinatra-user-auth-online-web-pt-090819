600f43a13bb56b4adc8379e390f1dfdfd5a6bf2a
Alexander Bispo <alexbispo173@gmail.com>
2020-01-13 15:09:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
@@ -45,8 +46,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
-  end
+    binding.pry
+  erb :'/users/home'
+end
 end
